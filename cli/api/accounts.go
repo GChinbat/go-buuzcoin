38e8f953360593d397b95884d58370fe3b00f02f
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-18 18:55:41 +0800

    fix: Check length of requested account address

--- cli/api/accounts.go
+++ cli/api/accounts.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/hex"
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/buuzcoin/go-buuzcoin/cli/chain"
@@ -11,18 +12,27 @@ import (
 // GetAccountState retrieves account state for current block.
 // API endpoint: /api/v1/account?address=<account address>
 func GetAccountState(w http.ResponseWriter, r *http.Request) {
-	accountAdderss := r.URL.Query().Get("address")
-	accountAdderss = accountAdderss[2:]
-	passAddress, err := hex.DecodeString(accountAdderss)
+	accountAddress := r.URL.Query().Get("address")
+
+	// len(accountAddress) == len('0x') + len(address)*2 as address is hex-encoded
+	if len(accountAddress) != 2+40 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	address, err := hex.DecodeString(accountAddress[2:])
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	accountState, err := chain.BlockchainDispatcher.GetAccountState(passAddress)
+
+	accountState, err := chain.BlockchainDispatcher.GetAccountState(address)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("BlockchainDispatcher.GetAccountState failed: %+v\n", err)
 		return
 	}
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(jsonObject{
 		"balance":      accountState.Balance,
